9cc0bcd4028fee16e29c863403698be148faf736
NickVeliki <nickveliki@gmail.com>
2020-04-09 16:00:16 +0200

    editAdmin

--- htdocs/app_41.js
+++ htdocs/app_41.js
@@ -7,3 +7,12 @@ const params = JSON.parse(new URLSearchParams(window.location.search).get("q"));
 console.log(params);
 document.getElementById("progressBar").setAttribute("style", `height: 25px; width: ${window.innerWidth/10*params.progress}px`);
 const question = JSON.parse(localStorage.getItem(params.main+params.modulename))[params.progress]||{};
+const adminSave = document.getElementById("adminSave");
+if(admin){
+    adminSave.setAttribute("style", "");
+    adminSave.addEventListener("click", (ev)=>{
+        ev.preventDefault();
+        gatherData(params.main+params.modulename+params.progress);
+    })
+}
+getData(params.main+params.modulename+params.progress);
--- htdocs/editAdmin.js
+++ htdocs/editAdmin.js
@@ -20,7 +20,7 @@ const select = (element, list, arg)=>{
 }
 const reveal = (editor)=>{
     
-    editor.setAttribute("style", editor.getAttribute("style").replace("display: none;", "")||"");
+    editor.setAttribute("style", editor.getAttribute("style").replace("display: none;", ""));
     console.log(editor);
     editConfirm.setAttribute("style", "");
     editCancel.setAttribute("style", "");
@@ -38,6 +38,21 @@ if(admin){
               select(editInput, selectedEditor);
               reveal(selectedEditor[0]);
             })
+            label.addEventListener("mouseenter", (ev)=>{
+                const style = ev.target.getAttribute("style").split(";");
+                let BackgroundColor;
+                style.forEach((obj)=>{
+                    if(obj.split(":")[0].trim()==="background-color"){
+                        BackgroundColor=obj.split(":")[1];
+                    } 
+                })
+                if(!BackgroundColor){
+                    ev.target.setAttribute("style", "background-color: #eeeecc;"+ev.target.getAttribute("style"))
+                }
+            })
+            label.addEventListener("mouseleave", (ev)=>{
+                ev.target.setAttribute("style", ev.target.getAttribute("style").replace("background-color: #eeeecc;", ""));
+            })
         }
     };
     for(let img of document.getElementsByTagName("img")){
@@ -46,6 +61,12 @@ if(admin){
                 select(img, SelectedElement);
                 select(addFile, selectedEditor)
                 reveal(selectedEditor[0]);
+            })
+            img.addEventListener("mouseenter", (ev)=>{
+                ev.target.setAttribute("style","opacity: 0.7;"+ev.target.getAttribute("style"))
+            })
+            img.addEventListener("mouseleave", (ev)=>{
+                ev.target.setAttribute("style", ev.target.getAttribute("style").replace("opacity: 0.7;", ""))
             })
         }
     }
@@ -56,6 +77,27 @@ if(admin){
                 select(editField, selectedEditor);
                 reveal(selectedEditor[0]);
             })
+            p.addEventListener("mouseenter", (ev)=>{
+                ev.target.setAttribute("style", "background-color: #eeeecc;"+ev.target.getAttribute("style"))
+            })
+            p.addEventListener("mouseleave", (ev)=>{
+                ev.target.setAttribute("style", ev.target.getAttribute("style").replace("background-color: #eeeecc;", ""))
+            })
+        }
+    }
+    for(let div of document.getElementsByTagName("div")){
+        if(div.id&&div.id.includes("edit")){
+            div.addEventListener("click", (ev)=>{
+                select(div, SelectedElement);
+                select(editField, selectedEditor);
+                reveal(selectedEditor[0]);
+            })
+            div.addEventListener("mouseenter", (ev)=>{
+                ev.target.setAttribute("style", "background-color: #eeeecc;"+ev.target.getAttribute("style"))
+            })
+            div.addEventListener("mouseleave", (ev)=>{
+                ev.target.setAttribute("style", ev.target.getAttribute("style").replace("background-color: #eeeecc;", ""))
+            })
         }
     }
     editConfirm.addEventListener("click", (ev)=>{
@@ -65,11 +107,62 @@ if(admin){
             fileReader.readAsDataURL(selectedEditor[0].files[0]);
             fileReader.onloadend = (ev)=>{
                 SelectedElement[0].src=ev.target.result;
+                selectedEditor[0].files[0]=undefined;
             }
         }else{
             SelectedElement[0][SelectedElement[1]||"innerHTML"]=selectedEditor[0][selectedEditor[1]||"value"];
-            selectedEditor[0][selectedEditor[1]||"value"]="";
         }
+        selectedEditor[0][selectedEditor[1]||"value"]="";
         hide(selectedEditor[0]);
+    })
+    editCancel.addEventListener("click", (ev)=>{
+        ev.preventDefault();
+        if(selectedEditor[0]==addFile){
+            selectedEditor[0].files[0]=undefined;
+        }
+        selectedEditor[0][selectedEditor[1]||"value"]="";
+        hide(selectedEditor[0])
+    })
+}
+const gatherData = (id)=>{
+    const data = [];
+    for (let label of document.getElementsByTagName("label")){
+        if(label.id){
+            data.push({id: label.id, attr: "innerHTML", content: label.innerHTML});
+        }
+    }
+    for (let p of document.getElementsByTagName("p")){
+        if(p.id){
+            data.push({id: p.id, attr: "innerHTML", content: p.innerHTML});
+        }
+    }
+    for (let img of document.getElementsByTagName("img")){
+        if(img.id){
+            data.oush({id: img.id, attr: "src", content: img.src});
+        }
+    }
+    for (let div of document.getElementsByTagName("div")){
+        if(div.id&&div.id.includes("edit")){
+            data.oush({id: div.id, attr: "innerHTML", content: div.src});
+        }
+    }
+    fetch("/text", {
+        method:"POST",
+        headers:{
+            "Authorization":`Bearer ${localStorage.getItem("token")}`,
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({id, content: data})
+    })
+}
+const getData = (id)=>{
+    fetch("/text?id="+id).then((response)=>{
+        if(response.status<400){
+            response.json().then(({content})=>{
+                content.forEach(({id, attr, content})=>{
+                    document.getElementById(id)[attr]=content
+                })
+            })
+        }
     })
 }
