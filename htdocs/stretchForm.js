3b90dc46a22aee2c509ff2d920e19387d02d5c39
NickVeliki <nickveliki@gmail.com>
2020-04-18 14:26:30 +0200

    arggggggghh

--- htdocs/stretchForm.js
+++ htdocs/stretchForm.js
@@ -1,1 +1,1 @@
-document.getElementById("container").setAttribute("style", `min-height: ${window.innerHeight*stretchFactor}px; ${document.getElementById("main").getAttribute("style")}`)
+document.getElementById("container").setAttribute("style", `min-height: ${window.innerHeight*stretchFactor}px; ${document.getElementById("container").getAttribute("style")}`)
