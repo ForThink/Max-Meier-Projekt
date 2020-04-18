fc5288bc3d89c14cb0cd7be495d5141eac2befb9
NickVeliki <nickveliki@gmail.com>
2020-04-18 13:53:56 +0200

    design oh

--- htdocs/stretchForm.js
+++ htdocs/stretchForm.js
@@ -1,1 +1,1 @@
-document.getElementById("main").setAttribute("style", `min-height: ${window.innerHeight}px; ${document.getElementById("main").getAttribute("style")}`)
+document.getElementById("container").setAttribute("style", `min-height: ${window.innerHeight*0.7}px; ${document.getElementById("main").getAttribute("style")}`)
