27460957a90144a0f15ab57e9245c97404494999
NickVeliki <nickveliki@gmail.com>
2020-04-19 13:55:30 +0200

    hidebuttons

--- htdocs/editAdmin.js
+++ htdocs/editAdmin.js
@@ -413,7 +413,7 @@ const getData = (id)=>new Promise((res, rej)=>{
                     const item = document.getElementById(id);
                     if(item){
                         item[attr]=content;
-                        if(style){
+                        if(style&&(admin||(!item.id.includes("admin")&&!item.id!="allezeigen")&&item.id!="a1"&&item.id!="a2"&&item.id!="a3")){
                             item.setAttribute("style", style);
                         }
                     }
