1c599e388a7a9310cc9ebfdbbf129e8b08c1dca5
NickVeliki <nickveliki@gmail.com>
2020-04-19 22:26:30 +0200

    sonvolnvolnfv

--- htdocs/progressBar.js
+++ htdocs/progressBar.js
@@ -3,4 +3,4 @@ document.getElementById("main").addEventListener("submit", (ev)=>{
     ev.preventDefault();
     window.location=`/3${3+group}.html`
 });
-document.getElementById("progressBar").setAttribute("style", `height: 25px; width: ${window.innerWidth/10*params.progress}px`);
+document.getElementById("progressBar").setAttribute("style", `height: 25px; width: ${window.innerWidth/10*(params.progress+1)}px`);
