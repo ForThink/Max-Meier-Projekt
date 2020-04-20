527fc433a6390a98c376730208b3fc00eef45e05
NickVeliki <nickveliki@gmail.com>
2020-04-20 15:28:09 +0200

    badge calculations

--- htdocs/app_35.js
+++ htdocs/app_35.js
@@ -34,32 +34,53 @@ let aplicado = 0;
 logdates.forEach((date) => {    
     const d1 = new Date(date[1])-0||Date.now();
     const d2 = new Date(date[0])-0;
-    if(!isNaN(d1)&&!isNaN(d2)){
-        aplicado+=d1-d2;
+    if(aplicado<6&&!isNaN(d1)&&!isNaN(d2)&&(d1-d2)>=1000*60*30){
+        aplicado++;
     }
-    
 });
-aplicado = Math.floor(Math.log2(aplicado/(1000*60*30)));
 let inteligente = 0;
 let estudioso = 0;
-const statsq = JSON.parse(localStorage.getItem("statQuestions")).map(({qid})=>qid);
-(xp||[]).forEach((day)=>{
-    day.xp.forEach(({qid, right})=>{
-        if(!statsq.includes(qid)){
+const checked = [];
+const estudiosointeligente = (modulename)=>{
+    const mod1m1= JSON.parse(localStorage.getItem(modulename))||[];
+    mod1m1.forEach(({qid})=>{
+        let intelligent = 0;
+        let studious = 0;
+        xp.forEach(({xp})=>{
+            xp.forEach((item)=>{
+                if(!checked.includes(qid)&&item.qid==qid){
+                    checked.push(qid);
+                    studious++;
+                    if(item.right){
+                        intelligent++;
+                    }
+                }
+            })
+        })
+        if(intelligent>7){
+            inteligente++;
+        }
+        if(studious==10){
             estudioso++;
-            if(right){
-                inteligente++;
-            }
         }
     })
-})
-const level = (inteligente, estudioso)=>{
-    return (isNaN(inteligente/estudioso)||inteligente/estudioso<0.55||estudioso<10)?0:Math.ceil((inteligente/estudioso-0.55)/0.15);
 }
-window.level = level;
-document.getElementById("aplicado").innerHTML=`Level ${aplicado<0?0:aplicado<3?aplicado:3}`;
-document.getElementById("inteligente").innerHTML=`Level ${level(inteligente, estudioso)}`
-document.getElementById("estudioso").innerHTML=`Level ${estudioso<10?0:estudioso<30?1:estudioso<60?2:3}`
+estudiosointeligente("Modul1mod1");
+estudiosointeligente("Modul1mod2");
+estudiosointeligente("Modul1mod3");
+estudiosointeligente("Modul2mod1");
+estudiosointeligente("Modul2mod2");
+estudiosointeligente("Modul2mod3");
+const level = (stat)=>{
+    let level = 0;
+    while(level*(level+1)<2*stat){
+        level++
+    }
+    return level;
+}
+document.getElementById("aplicado").innerHTML=`Level ${level(aplicado)}`;
+document.getElementById("inteligente").innerHTML=`Level ${level(inteligente)}`
+document.getElementById("estudioso").innerHTML=`Level ${level(estudioso)}`
 if(admin){
     const editAdmin = document.getElementById("edit_admin");
     editAdmin.setAttribute("style", "");
